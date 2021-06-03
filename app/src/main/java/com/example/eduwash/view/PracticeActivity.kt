e64899e4c7deda3c32cc2461e7af979342f1c847
mridwanslubis <mridwanslubis@gmail.com>
2021-06-03 16:25:48 +0700

    Complete practice page

--- app/src/main/java/com/example/eduwash/view/PracticeActivity.kt
+++ app/src/main/java/com/example/eduwash/view/PracticeActivity.kt
@@ -10,11 +10,10 @@ import androidx.viewpager.widget.ViewPager
 import com.example.eduwash.MainActivity
 import com.example.eduwash.R
 import com.example.eduwash.config.AppPrefs
+import com.example.eduwash.databinding.ActivityPracticeBinding
 import com.example.eduwash.hide
 import com.example.eduwash.show
 import com.google.firebase.storage.FirebaseStorage
-import kotlinx.android.synthetic.main.activity_on_boarding.nextBtn
-import kotlinx.android.synthetic.main.activity_practice.*
 import kotlinx.android.synthetic.main.practice_slide1.*
 import kotlinx.android.synthetic.main.practice_slide2.*
 import kotlinx.android.synthetic.main.practice_slide3.*
@@ -22,6 +21,8 @@ import pl.droidsonroids.gif.GifDrawable
 import java.io.File
 
 class PracticeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPracticeBinding
 
     private lateinit var sliderAdapter: SliderAdapter
     private var dots: Array<TextView?>? = null
@@ -38,13 +39,13 @@ class PracticeActivity : AppCompatActivity() {
         override fun onPageSelected(position: Int) {
             addBottomDots(position)
             if (position == layouts.size.minus(1)) {
-                nextBtn.hide()
-                prevBtn.hide()
-                homeBtn.show()
+                binding.nextBtn.hide()
+                binding.prevBtn.hide()
+                binding.homeBtn.show()
             } else {
-                nextBtn.show()
-                prevBtn.show()
-                homeBtn.hide()
+                binding.nextBtn.show()
+                binding.prevBtn.show()
+                binding.homeBtn.hide()
             }
         }
 
@@ -56,7 +57,10 @@ class PracticeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_practice)
+        binding = ActivityPracticeBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        //setContentView(R.layout.activity_practice)
 
 
         retrieveGifAsset()
@@ -66,6 +70,7 @@ class PracticeActivity : AppCompatActivity() {
     }
 
     private fun retrieveGifAsset() {
+
         val gifImage = FirebaseStorage.getInstance().reference.child("gif/step1.gif")
         val gifImage2 = FirebaseStorage.getInstance().reference.child("gif/step2_left.gif")
         val gifImage2Right = FirebaseStorage.getInstance().reference.child("gif/step2_right.gif")
@@ -87,12 +92,15 @@ class PracticeActivity : AppCompatActivity() {
             Toast.makeText(this, "Failed to retrieve the image", Toast.LENGTH_SHORT).show()
         }
 
+        /*
         gifImage2Right.getFile(localFile2Right).addOnSuccessListener {
             val gif2RightFromPath = GifDrawable(localFile2Right.absolutePath)
             img2Right.setImageDrawable(gif2RightFromPath)
         }.addOnFailureListener {
             Toast.makeText(this, "Failed to retrieve the image", Toast.LENGTH_SHORT).show()
         }
+
+         */
     }
 
     private fun init() {
@@ -100,14 +108,22 @@ class PracticeActivity : AppCompatActivity() {
             R.layout.practice_slide1,
             R.layout.practice_slide2,
             R.layout.practice_slide3,
-                R.layout.practice_slide4
+                R.layout.practice_slide4,
+                R.layout.practice_slide5,
+                R.layout.practice_slide6,
+                R.layout.practice_slide7,
+                R.layout.practice_slide8,
+                R.layout.practice_slide9,
+                R.layout.practice_slide10,
+                R.layout.practice_slide11,
+                R.layout.practice_slide12
         )
         sliderAdapter = SliderAdapter(this, layouts)
     }
 
     private fun dataSet() {
         addBottomDots(0)
-        sliderPractice.apply {
+        binding.sliderPractice.apply {
             adapter = sliderAdapter
             addOnPageChangeListener(sliderChangeListener)
         }
@@ -116,13 +132,13 @@ class PracticeActivity : AppCompatActivity() {
     private fun addBottomDots(currentPage: Int) {
         dots = arrayOfNulls(layouts.size)
 
-        dotsPracticeLayout.removeAllViews()
+        binding.dotsPracticeLayout.removeAllViews()
         for (i in 0 until dots!!.size) {
             dots!![i] = TextView(this)
             dots!![i]?.text = Html.fromHtml("&#8226;")
             dots!![i]?.textSize = 35f
             dots!![i]?.setTextColor(resources.getColor(R.color.colorGrey))
-            dotsPracticeLayout.addView(dots!![i])
+            binding.dotsPracticeLayout.addView(dots!![i])
         }
 
         if (dots!!.isNotEmpty()) {
@@ -131,22 +147,22 @@ class PracticeActivity : AppCompatActivity() {
     }
 
     private fun interaction() {
-        prevBtn.setOnClickListener {
+        binding.prevBtn.setOnClickListener {
             val current = getCurrentScreen(-1)
             if (current < layouts.size) {
-                sliderPractice.currentItem = current
+                binding.sliderPractice.currentItem = current
             } else {
                 navigateToHome()
             }
         }
-        homeBtn.setOnClickListener {
+        binding.homeBtn.setOnClickListener {
             // Launch login screen
             navigateToHome()
         }
-        nextBtn.setOnClickListener {
+        binding.nextBtn.setOnClickListener {
             val current = getCurrentScreen(+1)
             if (current < layouts.size) {
-                sliderPractice.currentItem = current
+                binding.sliderPractice.currentItem = current
             } else {
                 navigateToHome()
             }
@@ -159,5 +175,5 @@ class PracticeActivity : AppCompatActivity() {
         finish()
     }
 
-    private fun getCurrentScreen(i: Int): Int = sliderPractice.currentItem.plus(i)
+    private fun getCurrentScreen(i: Int): Int = binding.sliderPractice.currentItem.plus(i)
 }
